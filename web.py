00bc7d56e2b0b8370de3b71a762ed234f7b9aac1
Tarikkkkkk <taursembinov@gmail.com>
2025-04-06 17:13:50 +0900

    Update web.py

--- web.py
+++ web.py
@@ -12,28 +12,25 @@ st.title("Heart Disease Prediction")
 st.subheader("Enter Patient Data:")
 
 age = st.slider("Age", 20, 80, 50)
-sex = st.radio("Sex", [0, 1])
-cp = st.selectbox("Chest Pain Type (cp)", [0, 1, 2, 3])
 trestbps = st.slider("Resting Blood Pressure", 80, 200, 120)
 chol = st.slider("Cholesterol", 100, 600, 200)
-fbs = st.radio("Fasting Blood Sugar > 120 mg/dl", [0, 1])
-restecg = st.selectbox("Resting ECG (restecg)", [0, 1, 2])
 thalach = st.slider("Max Heart Rate Achieved (thalach)", 70, 210, 150)
 exang = st.radio("Exercise Induced Angina", [0, 1])
 oldpeak = st.slider("Oldpeak", 0.0, 6.0, 1.0)
-slope = st.selectbox("Slope of ST segment", [0, 1, 2])
-ca = st.selectbox("Number of major vessels (0-3)", [0, 1, 2, 3])
-thal = st.selectbox("Thalassemia", [0, 1, 2])
+thal_2 = st.radio("Thal_2", [0, 1])
+thal_3 = st.radio("Thal_3", [0, 1])
+cp_0 = st.radio("Chest Pain Type 0 (cp_0)", [0, 1])
+ca_0 = st.radio("Number of major vessels 0 (ca_0)", [0, 1])
 
-input_data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach,
-                        exang, oldpeak, slope, ca, thal]])
+input_data = np.array([[exang, age * chol, chol, trestbps, age, oldpeak,
+                        thal_3, cp_0, ca_0, thalach, thal_2]])
 
-input_data = scaler.transform(input_data)
+input_data[:, [1, 2, 3, 9]] = scaler.transform(input_data[:, [1, 2, 3, 9]])
 
 if st.button("Predict"):
     prediction = model.predict(input_data)[0]
     prob = model.predict_proba(input_data)[0][1]
-    
+
     if prediction == 1:
         st.error(f"High risk of heart disease with a {prob:.2f} chance.")
     else:
